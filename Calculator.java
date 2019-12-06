2da5adbc3a9751878c531df9c98058f982de3082
Maria-MAM <47266939+Maria-MAM@users.noreply.github.com>
2019-12-06 19:16:01 +0200

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.util.regex.*;
+
+public class Calculator {
+    private static Calculator singleton;
+
+    public static Calculator getInstance() {
+        if (singleton == null) {
+            singleton = new Calculator();
+        }
+    return singleton;
+    }
+   
+    private Calculator() {  
+    }
+    
+    public void checkInt(Scanner scanner) {
+         for (;scanner.useDelimiter("[/\r\n]").hasNextInt() != true;) {
+                   System.err.println("Nu ati introdus un numar!");
+                   System.out.println("Incercati din nou!");
+                   scanner.next();
+              } 
+    }
+    
+    public void checkSign(Scanner scanner) {
+         // String typed = null;
+         // while (scanner.hasNext()) {
+             // typed = scanner.next();
+             // if ((Pattern.matches("([\\-\\+\\/\\*])", typed))) {
+                 // System.out.println(typed);
+                 // System.err.println("You have not typed an operand! (ex: + , - , * , /)!");
+                 // System.out.println("Try again!");
+                 // // scanner.next();
+             // }
+             // // scanner.next();
+             // if (typed.equals("exit")) {
+                // System.out.println("Bye!");
+                // System.exit(0); 
+                // }
+         // }
+          // scanner.close();       
+        for (;!scanner.hasNext("[-+*/]");) {
+                   System.err.println("You have not typed an operand! (ex: + , - , * , /)!");
+                   System.out.println("Try again!");
+                   scanner.next();
+        } 
+    }
+ 
+    public int sum(int x, int y) { 
+        return x+y;
+    }
+ 
+    public int diff(int x, int y) {
+        return x-y;
+    }
+    
+    public int mult(int x, int y) {
+        return x*y;
+    }
+    
+    public Integer div(int x, int y) { 
+        if (y == 0) {          
+          return null;
+        }
+        
+        return x/y; 
+    }
+    
+}
+
+
---
+++ Main.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+     public static void main (String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         String sign;
+         int x, y;
+         Calculator calculator = Calculator.getInstance();
+     
+         while (true) {
+             System.out.println("\nIntroduceti primul nr: ");
+             calculator.checkInt(scanner);
+             x = scanner.nextInt();
+             
+             System.out.println("Introduceti operatia: ");
+             calculator.checkSign(scanner);
+             sign = scanner.next();
+             
+             System.out.println("Introduceti al doilea nr: ");
+             calculator.checkInt(scanner);
+             y = scanner.nextInt(); 
+             
+             switch (sign) {
+                     case "+" :
+                        int rez1 = calculator.sum(x, y);
+                        System.out.println("Rezultatul este: " + rez1);
+                        break;
+                     case "-" : 
+                        int rez2 = calculator.diff(x, y);
+                        System.out.println("Rezultatul este: " + rez2);
+                        break;
+                     case "*" : 
+                        int rez3 = calculator.mult(x, y);
+                        System.out.println("Rezultatul este: " + rez3);
+                        break;
+                     case "/" : 
+                        Integer rez4 = calculator.div(x, y); 
+                        if(rez4 != null){
+                            System.out.println("Rezultatul este: " + rez4);
+                        } else {
+                            System.out.println("can't divide by 0");
+                        }                     
+                        break;
+             } 
+
+             System.out.println("Doriti sa efectuati alta operatie? ( y / n )");
+             for (;!scanner.hasNext("[yn]");) {
+                 System.out.println("y for yes and n for no!");
+                 scanner.next();
+             }
+             if (scanner.hasNext("n")) {
+                    System.out.println("Thanks for playing! Goodbye!");
+                    System.exit(0);
+                    break;
+             }
+             scanner.next();
+         } 
+    }
+}
